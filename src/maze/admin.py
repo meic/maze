8c7f70f662199ee3406828de21ac49eee633fe9c
Meic Williams <1534923+meic@users.noreply.github.com>
2022-03-05 15:20:14 +0000

    Remove cell inlne as performace of admin too slow

--- src/maze/admin.py
+++ src/maze/admin.py
@@ -3,15 +3,9 @@ from django.contrib import admin
 from . import models
 
 
-class CellInline(admin.TabularInline):
-    model = models.Cell
-
-
 @admin.register(models.Maze)
 class MazeAdmin(admin.ModelAdmin):
-    inlines = [
-        CellInline,
-    ]
+    pass
 
 
 @admin.register(models.Step)
